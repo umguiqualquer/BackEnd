a1fae2450c1ad7dd5644f72eb54b3f7aa579edfc
GuilhermeCamposdeAlmeida <guilherme.campos.senai404@gmail.com>
2024-08-02 10:32:02 -0300

    Arquivos  da Atividade usando " e BR

---
+++ Php/OlaMundo.php
@@ -0,0 +1,8 @@
+<?php
+// A baixo estou escrevendo uma linha de texto
+echo 'Olá Mundo!<br>';
+echo 'Bem Vindo ao SESI SENAI<br>';/*
+Aqui eu usei o BR para pular as linhas*/
+echo 'Cerquilho - SP';
+
+?>
---
+++ Php/index.php
@@ -0,0 +1,26 @@
+<?php
+//Abaixo declarei as variaves que vão ser ultilizadas no código
+$nome = 'Guilherme Campos';
+$escola = 'SESI Cerquilho';
+$cidade = 'Cerquilho';
+$CE = 428;
+$curso = 'Desenvolvimento de Sistemas';
+$media = 9.5;
+
+//Ultilizei Echo com ' para escrever e usar o BR dentro da escrita
+echo 'Olá bem vindo ao SESI-SP <br>';
+
+//Usei Echo para escrever os textos, ultilizei as " para usar o BR na variavel echo em texto
+
+echo "$nome <br>";
+echo "$escola <br>";
+echo "$cidade <br>";
+echo "$CE <br>";
+echo "$curso <br>";
+echo "$media <br>";
+
+//Texto de demonstração com "
+
+echo "Olá $nome, seja bem vindo ao SESI $CE de $cidade, sua nota do curso de $curso é $media"
+
+?>
