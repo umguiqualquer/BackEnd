a397918de058d9ae7f2471b000d056db650e59f7
GuilhermeCamposdeAlmeida <guilherme.campos.senai404@gmail.com>
2024-08-07 15:26:47 -0300

    atividade if e else

--- ifelse.php
+++ ifelse.php
@@ -1,9 +1,17 @@
 <?php
+//declarei que se 9 for igual a nove ele ira escrever um texto caso contrario ele escrevera outro
 
-if (9 == 1){
-    echo 'Os dois numeros são iguais';
+if (9 == 9){
+    echo 'Os dois numeros são iguais <br>';
 }else{
-    echo 'Os dois numeros não são iguais';
+    echo 'Os dois numeros são diferentes <br>';
+}
+
+//declarei que se 9 for EXATAMENTE ate os tipos dos numeros igual a 9.0 ele ira escrever um texto caso contrario ele escrevera outro
+if (9 === 9.0){
+    echo 'Os doi numeros são iguais';
+}else{
+    echo 'Os dois numeros dão diferentes';
 }
 
 ?>
